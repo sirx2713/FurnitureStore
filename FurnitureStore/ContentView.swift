f02f4c170284f1cd417583654335b3a64e6c6a13
iSAD <bradx2713@gmail.com>
2024-08-21 12:42:46 +0200

    Initial Commit

---
+++ FurnitureStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FurnitureStore
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FurnitureStore/FurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureStoreApp.swift
+//  FurnitureStore
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
